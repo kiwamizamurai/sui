b51729befee1dd6012c263358e3dcc27f382e634
Ashok Menon <ashok@mystenlabs.com>
2025-01-17 18:36:57 +0000

    indexer-alt: kv_checkpoints split out signatures
    ## Description
    
    Split out `kv_checkpoint`'s `certified_checkpoint` field into a
    `checkpoint_summary` and its `signatures`, to match the schema used by
    the KV store.
    
    ## Test plan
    
    Run the indexer and inspect the results.

--- crates/sui-indexer-alt-schema/src/checkpoints.rs
+++ crates/sui-indexer-alt-schema/src/checkpoints.rs
@@ -13,10 +13,12 @@ use crate::schema::{kv_checkpoints, kv_genesis};
 #[diesel(table_name = kv_checkpoints)]
 pub struct StoredCheckpoint {
     pub sequence_number: i64,
-    /// BCS serialized CertifiedCheckpointSummary
-    pub certified_checkpoint: Vec<u8>,
     /// BCS serialized CheckpointContents
     pub checkpoint_contents: Vec<u8>,
+    /// BCS serialized CheckpointSummary
+    pub checkpoint_summary: Vec<u8>,
+    /// BCS serialized AuthorityQuorumSignInfo
+    pub raw_signatures: Vec<u8>,
 }
 
 #[derive(Insertable, Selectable, Queryable, Debug, Clone)]
